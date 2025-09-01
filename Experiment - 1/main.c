cbf0856f2bcd56f8d926092678a7d728d451c78b
hansikag22-del <hansikag22@gmail.com>
2025-09-01 09:59:08 +0530

    Initial Commit

---
+++ Experiment - 1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Experiment - 1
+//
+//  Created by Hansika Gupta on 01/09/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
